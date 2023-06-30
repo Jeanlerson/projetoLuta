7f7975f5384c9cb8c655275111ce16956549ef05
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2023-06-30 09:04:31 -0300

    Classes-PersonagensMonster

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 let char = new knight('Jeanlerson');
+let monster = new BigMonster()
 
 console.log(char.name)
 console.log(char.attack)
 console.log(char.life)
+
+console.log(monster.name)
+console.log(monster.attack)
